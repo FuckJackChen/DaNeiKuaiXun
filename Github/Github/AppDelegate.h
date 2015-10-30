670a7621164245235404c7f23acfd3b7e7994821
FuckJackChen <1486872051@qq.com>
2015-10-30 11:18:31 +0800

    asdfgh
    愤怒吧

---
+++ Github/Github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github
+//
+//  Created by soft on 15/10/30.
+//  Copyright © 2015年 soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
